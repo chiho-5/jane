267c1c103e8a26893fe0600d13ecfd30b8e6028d
Ezeora Nnenna Esther <71467633+chiho-5@users.noreply.github.com>
2025-01-06 07:25:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 pip install torch==2.5.1 --index-url https://download.pytorch.org/whl/cpu 
 pip install --no-deps sentence-transformers
-pip install  Pillow scikit-learn scipy transformers uvicorn
+pip install  Pillow scikit-learn scipy transformers uvicorn python-multipart 
 pip install llama-index llama-index-core llama-index-readers-web llama-index-embeddings-huggingface llama-index-llms-huggingface-api llama-index-llms-huggingface fastapi
