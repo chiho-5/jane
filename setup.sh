31d046c5c6a4b2f3a098e3462506769dfb698451
Ezeora Nnenna Esther <71467633+chiho-5@users.noreply.github.com>
2025-01-06 07:07:48 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,2 @@
+pip install torch==2.5.1 --index-url https://download.pytorch.org/whl/cpu sentence-transformers
+pip install llama-index llama-index-core llama-index-readers-web llama-index-embeddings-huggingface llama-index-llms-huggingface-api llama-index-llms-huggingface fastapi
